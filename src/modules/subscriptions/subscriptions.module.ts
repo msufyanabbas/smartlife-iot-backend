be7350b6b4e4eb18bce350910d5806874a5e58d2
Muhammad Sufyan Abbas <msufyanabbas10@gmail.com>
2026-04-08 16:34:40 +0300

    subscription added.

--- src/modules/subscriptions/subscriptions.module.ts
+++ src/modules/subscriptions/subscriptions.module.ts
@@ -3,18 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
 import { Subscription } from './entities/subscription.entity';
-import { Device, EmailTemplate, Payment, Tenant, User } from '../index.entities';
-import { UsersService } from '../users/users.service';
-import { MailService } from '../mail/mail.service';
-import { EmailTemplatesService } from '../email-templates/email-templates.service';
+import { Device, EmailTemplate, Payment, Tenant } from '../index.entities';
 import { InvoicePdfService } from './invoice-pdf.service';
-import { UsersModule } from '../index.module';
+import { UsersModule } from '../users/users.module';       // ← direct path, not index.module
+import { MailModule } from '../mail/mail.module';
+import { EmailTemplatesModule } from '../email-templates/email-templates.module';
 
-@Global() // Make it globally available
+@Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Subscription, User, Tenant, Device, EmailTemplate, Payment]), forwardRef(() => UsersModule)],  // ← import the whole module, not just the service],
+  imports: [
+    TypeOrmModule.forFeature([Subscription, Tenant, Device, EmailTemplate, Payment]),
+    forwardRef(() => UsersModule),      // ← provides UsersService with all its deps
+    MailModule,                          // ← provides MailService
+    EmailTemplatesModule,                // ← provides EmailTemplatesService
+  ],
   controllers: [SubscriptionsController],
-  providers: [SubscriptionsService, UsersService, MailService, EmailTemplatesService, InvoicePdfService],
-  exports: [SubscriptionsService, UsersService, MailService, EmailTemplatesService],
+  providers: [
+    SubscriptionsService,
+    InvoicePdfService,
+    // ← UsersService, MailService, EmailTemplatesService removed from here
+  ],
+  exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
